7eaf0ddb221f4c9816767920dda395d7f37fe696
James Declerus <james.declerus@esih.edu>
2019-06-28 14:49:22 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,11 @@
-class Puppy 
-  
+class Puppy
+
   attr_accessor :name, :breed, :age
-  
+
   def initialize(name, breed, age)
     @name = name
-    @breed = breed 
+    @breed = breed
     @age = age
   end
-  
+
 end
