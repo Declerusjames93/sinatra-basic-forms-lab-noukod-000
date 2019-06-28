81bb44f9269df08c80ad7dd3bd0a4eb7eaaa7ebd
James Declerus <james.declerus@esih.edu>
2019-06-28 14:52:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,18 @@
 require_relative 'config/environment'
+
 class App < Sinatra::Base
-get '/'do
-erb:index
-end
-get '/'do
-erb:create_puppy
-end
-get '/'do
-@puppy = Puppy.new(params[:name], params[:breed], params[:age])
-erb:display_puppy
-end
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  get '/new' do 
+    erb :create_puppy
+  end
+  
+  post '/' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+
 end
